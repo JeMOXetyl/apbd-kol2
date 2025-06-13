d93adc0a621152a644bf3001af298cc4bd367009
Zofia Chmielewska <s29815@pjwstk.edu.pl>
2025-06-13 15:31:16 +0200

    Zadanie

--- KolE/KolE/Data/DatabaseContext.cs
+++ KolE/KolE/Data/DatabaseContext.cs
@@ -15,6 +15,41 @@ public class DatabaseContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Employee>().HasData(new List<Employee>
+        {
+            new Employee()
+                { EmployeeId = 1, FirstName = "Anna", LastName = "Kowalska", HireDate = DateTime.Now.AddYears(-20) },
+            new Employee()
+                { EmployeeId = 1, FirstName = "Jan", LastName = "Nowak", HireDate = DateTime.Now.AddYears(-20) }
+        });
+
+        modelBuilder.Entity<Nursery>().HasData(new Nursery
+        {
+            NurseryId = 1,
+            Name = "Green Forest Nursery",
+            EstablilishedDate = DateTime.Now.AddYears(-20)
+        });
+
+        modelBuilder.Entity<TreeSpecies>().HasData(new TreeSpecies
+        {
+            SpeciesId = 1,
+            LatinName = "Quercus robur",
+            GrowthTimeInYears = 5
+        });
         
-    }
+        modelBuilder.Entity<SeedlingBatches>().HasData(new SeedlingBatch()
+        {
+            BatchId = 1,
+            NurseryId = 1,
+            SpeciesId = 1,
+            Quantity = 500,
+            SownDate = DateTime.Now.AddYears(-20),
+            ReadyDate = DateTime.Now.AddYears(-10)
+        });
+
+        modelBuilder.Entity<Responsibles>().HasData(new List<Responsible>
+        {
+            new Responsible() { 1, 1, "Supervisor" },
+            new Responsible() { 1, 2, "Planter" }
+        });
 }
